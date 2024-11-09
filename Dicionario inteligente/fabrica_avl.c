19c1ef56764d944b29f3980acb6296c971573dec
juliaChaparro <julia.ferreira@icomp.ufam.edu.br>
2024-11-09 02:01:59 -0400

    finalmente ta quase completpt

--- Dicionario inteligente/fabrica_avl.c
+++ Dicionario inteligente/fabrica_avl.c
@@ -14,7 +14,7 @@ typedef struct {
     double Flow_Rate;
     double RPM;
     double Operational_Hours;
-    double Maintenance_Flag; 
+    int Maintenance_Flag; 
 
 } t_sensores;
 
@@ -41,7 +41,7 @@ int comparar_bomba(t_bomba* bomba1,t_bomba* bomba2){
 }
 
 void imprimir_sensores(t_sensores* senso){
-    printf("Class_ID: %d\n Temperature: %.4f\n Vibration: %.4f\n Pressure: %.4f\n Flow_Rate: %.4f\n RPM: %.4f\n Operational_Hours: %.4f\n Maintenance_Flag: %.4d\n",senso->Class_ID,senso->Temperature,senso->Vibration,senso->Pressure,senso->Flow_Rate,senso->RPM, senso->Operational_Hours,senso->Maintenance_Flag);
+    printf("Class_ID: %d\n Temperature: %.4f\n Vibration: %.4f\n Pressure: %.4f\n Flow_Rate: %.4f\n RPM: %.4f\n Operational_Hours: %.4f\n Maintenance_Flag: %d\n",senso->Class_ID,senso->Temperature,senso->Vibration,senso->Pressure,senso->Flow_Rate,senso->RPM, senso->Operational_Hours,senso->Maintenance_Flag);
 }
 
 int comparar_sensores(t_sensores *s1, t_sensores *s2){
@@ -194,6 +194,85 @@ void report_mean(FILE* arq,t_fabrica* fabi, int chave){
 
 }
 
+void report_max(FILE* arq,t_fabrica* fabi,int chave){
+    t_bomba bom;
+    bom.Pump_ID = chave;
+    t_sensores* senso;
+    double maior_pressure;
+    double maior_Temperature;
+    double maior_Vibration;
+
+    
+    t_bomba* result= buscar_avl(fabi->avl,&chave);
+    int tam = tamanho_lse(result->lse);
+    if(result){
+        for(int i=1;i<=tam;i++){
+            senso = acessar_lse(result->lse,i);
+            if(i == 1){
+                maior_pressure = senso->Pressure;
+                maior_Temperature=senso->Temperature;
+                maior_Vibration =senso->Vibration;
+            }
+                if (senso->Pressure > maior_pressure){
+                    maior_pressure = senso->Pressure;
+                }
+                if(senso->Vibration > maior_Vibration){
+                    maior_Vibration = senso->Vibration;
+                }
+                if(senso->Temperature > maior_Temperature){
+                    maior_Temperature = senso->Temperature;
+                }
+
+        }
+        fprintf(arq,"maior tem Temperature: %lf\n",maior_Temperature);
+        fprintf(arq,"maior tem Vibration: %lf\n",maior_Vibration);
+        fprintf(arq,"maior tem Pressure: %lf\n",maior_pressure);
+    }
+    else{
+        fprintf(arq,"nao encontrou o id\n");
+    }
+
+}
+
+void report_min(FILE* arq,t_fabrica* fabi,int chave){
+    t_bomba bom;
+    bom.Pump_ID = chave;
+    t_sensores* senso;
+    double menor_pressure;
+    double menor_Temperature;
+    double menor_Vibration;
+
+    
+    t_bomba* result= buscar_avl(fabi->avl,&chave);
+    int tam = tamanho_lse(result->lse);
+    if(result){
+        for(int i=1;i<=tam;i++){
+            senso = acessar_lse(result->lse,i);
+            if(i == 1){
+                menor_pressure = senso->Pressure;
+                menor_Temperature=senso->Temperature;
+                menor_Vibration =senso->Vibration;
+            }
+                if (senso->Pressure < menor_pressure){
+                    menor_pressure = senso->Pressure;
+                }
+                if(senso->Vibration < menor_Vibration){
+                    menor_Vibration = senso->Vibration;
+                }
+                if(senso->Temperature < menor_Temperature){
+                    menor_Temperature = senso->Temperature;
+                }
+
+        }
+        fprintf(arq,"menor tem Temperature: %lf\n",menor_Temperature);
+        fprintf(arq,"menor tem Vibration: %lf\n",menor_Vibration);
+        fprintf(arq,"menor tem Pressure: %lf\n",menor_pressure);
+    }
+    else{
+        fprintf(arq,"nao encontrou o id\n");
+    }
+
+}
 
 t_fabrica* criar_fabrica(){
     t_fabrica* fabi = malloc(sizeof(t_fabrica));
@@ -208,6 +287,8 @@ t_fabrica* abrir_fabrica(char nome_entrada[], char nome_saida[]){
     fabi=criar_fabrica();
 
     char comando[50];
+    char comando_REPORT[50];
+    
     int Pump_ID;
     int Class_ID;
     double Temperature;
@@ -242,20 +323,25 @@ t_fabrica* abrir_fabrica(char nome_entrada[], char nome_saida[]){
             remover(arq_saida,fabi,Pump_ID);
         }
         
-        else if (strcmp(comando, "REPORT MEAN") == 0){
-            fscanf(arq,"%d%*c", &Pump_ID);
-            report_mean(arq_saida,fabi,Pump_ID);
+        else if (strcmp(comando, "REPORT") == 0){
+            if(fscanf(arq," %49[^ ]", comando_REPORT) == 1){
+                if (strcmp(comando_REPORT, "MEAN") == 0){
+                    fscanf(arq,"%d%*c", &Pump_ID);
+                    report_mean(arq_saida,fabi,Pump_ID);
+                }
+                
+                else if (strcmp(comando_REPORT, "MAX") == 0){
+                    fscanf(arq,"%d%*c", &Pump_ID);
+                    report_max(arq_saida,fabi,Pump_ID);
+                }
+                
+                else if (strcmp(comando_REPORT, "MIN") == 0){
+                    fscanf(arq,"%d%*c", &Pump_ID);
+                    report_min(arq_saida,fabi,Pump_ID);
+                }
+            }
         }
-        /*
-        else if (strcmp(comando, "REPORT MIN") == 0){
-            fscanf(arq,"[^ ]%*d", Pump_ID);
-            //report_min(fabi,Pump_ID); tenho que fazer essas funçoes 
-        }
-        else if (strcmp(comando, "REPORT MAX") == 0){
-            fscanf(arq,"[^ ]%*d", Pump_ID);
-            //report_max(fabi,Pump_ID); tenho que fazer essas funçoes 
-        }
-        */
+
         else if(strcmp(comando,"END")==0){
             // tlz eu faça a funçao end "so tlz"
             fprintf(arq_saida,"end\n");
